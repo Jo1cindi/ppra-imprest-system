a4bdb44d37f62e1eca76ccdb618cd9c533d4ed51
Jo1cindi <joancindi535@gmail.com>
2022-10-06 12:16:23 +0300

    empty receipts page completion

--- src/Dashboard/EmployeeDashboard/Receipts.js
+++ src/Dashboard/EmployeeDashboard/Receipts.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import EmployeeSidebar from '../../Components/EmployeeSidebar';
+import Animation from "../../Images/EmptyReqNot.json"
+import Lottie from "react-lottie-player";
 
 const Receipts = () => {
+  
+  const receipts = ""
+  const showReceipts = () =>{
+    if(receipts){
+      return(
+        <div className='viewReceipts'>
+         
+        </div>
+      )
+    }else{
+      return(
+        <div className='emptyReceiptsPage'>
+         <div className="recAnimation">
+          <Lottie
+              loop
+              animationData={Animation}
+              play
+            />
+          </div>
+          <div className='emptyRecPageCard'>
+           <h4>You do not have any notifications yet.</h4>
+          </div>
+        </div>
+      )
+    }
+  }
+  
   return (
     <div className= "receipts">
      <EmployeeSidebar/>
      <div className='receiptsContent'>
-      Receipts Content
+       {showReceipts()}
      </div>
     </div>
   )
