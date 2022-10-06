a1667465fad6c016400b627729adb56732f15b0e
Jo1cindi <joancindi535@gmail.com>
2022-10-06 11:38:34 +0300

    Creating and empty requests page

--- src/Dashboard/EmployeeDashboard/ViewRequests.js
+++ src/Dashboard/EmployeeDashboard/ViewRequests.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import EmployeeSidebar from '../../Components/EmployeeSidebar'
+import Animation from "../../Images/EmptyRec.json"
+import Lottie from "react-lottie-player";
 
 const ViewRequests = () => {
-  return (
+  const request = ""
+  const showRequests = () =>{
+    if(request){
+      return(
+        <div className='requestsPage'></div>
+      )
+    }else{
+      return(
+        <div className='emptyRequestPage'>
+          <div className="reqnotAnimation">
+          <Lottie
+              loop
+              animationData={Animation}
+              play
+            />
+          </div>
+          <div className='emptyReqPageCard'>
+           <h4>You have not made any requests yet.</h4>
+          </div>
+        </div>
+      )
+    }
+  }
+return (
     <div className="viewRequests">
      <EmployeeSidebar/>
      <div className='viewRequestsContent'>
-      View Requests
+      {showRequests()}
      </div>
     </div>
   )
