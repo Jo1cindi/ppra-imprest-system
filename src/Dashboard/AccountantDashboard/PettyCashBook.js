bc26b4d4df22e14fc2a66d55741f11444ca530ee
Jo1cindi <joancindi535@gmail.com>
2022-11-09 11:51:07 +0300

    set initial amount card

--- src/Dashboard/AccountantDashboard/PettyCashBook.js
+++ src/Dashboard/AccountantDashboard/PettyCashBook.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "../DashboardStyles.css";
+import AccountantSidebar from "../../Components/AccountantSidebar";
+import { IoAdd,IoCloseSharp } from 'react-icons/io5';
+import { TextField } from "@mui/material";
+
+
 
 const PettyCashBook = () => {
+
+  const [initialAmountWindow, setInitialAmountWindow] = useState(false)
+  const [amount, setAmount] = useState()
+
+  let inputError = ""
+  const regex = /^\d+$/
+  if(regex.test(amount)=== false){
+    inputError = "Only enter numerical digits"
+  }else{
+    inputError= ""
+  } 
+  if(!amount){
+    inputError = ""
+  }
+
   return (
-    <div>PettyCashBook</div>
+    <>
+      {
+         initialAmountWindow && (
+          <div className='initialAmountWindow'>
+          <IoCloseSharp
+                className="closeReceiptCard"
+                onClick={() => setInitialAmountWindow(false)}
+              />
+           <div className='initialAmountCard'>
+            <h4>Set Intial Petty Cash Amount for the Month</h4>
+            <div className='amountInput'>
+              <TextField
+                variant='outlined'
+                className='amountInputField'
+                label="Enter amount"
+                type= "tel"
+                value={amount}
+                onChange={(e)=> setAmount(e.target.value)}
+                helperText={inputError}
+                FormHelperTextProps={
+                regex.test(amount) === false
+                  ? { style: { color: "red" } }
+                  : { style: { color: "green" } }
+              }
+              />
+            </div>
+            <input type="submit" value = "Set Amount" className='setAmountBtn'/>
+           </div>
+          </div>
+         )
+      } 
+      <AccountantSidebar/>
+      <div className="pettyCash">
+       <h3>Petty Cash Records</h3>
+       <div className="setInitialAmount" onClick={()=> {setInitialAmountWindow(true)}}>
+        <IoAdd className='addIcon'/>
+        <p>Set initial amount</p>
+       </div>
+      </div>
+    </>
   )
 }
 
